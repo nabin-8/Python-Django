103a12504aa6e0d026cf70a60830e0fbf2e0e9b0
nabin-8 <nabinacharya740@gmail.com>
2024-10-06 01:07:23 +0545

    primitive

---
+++ Complete_Python_Django/Python/02_Primitive_Types.py
@@ -0,0 +1,10 @@
+# primitive data_types
+students_count=1000
+rating=4.99
+is_published = True
+course_name = 'Python Programming'
+
+print(students_count, type(students_count))
+print(rating, type(rating))
+print(is_published, type(is_published))
+print(course_name, type(course_name))
