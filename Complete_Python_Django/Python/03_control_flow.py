b33e881a95918511a02559dad51486c33a6ef4b3
nabin-8 <nabinacharya740@gmail.com>
2024-10-06 22:16:40 +0545

    control flow

--- Complete_Python_Django/Python/03_control_flow.py
+++ Complete_Python_Django/Python/03_control_flow.py
@@ -1,10 +1,65 @@
+# Conditional statement
+# temperature =35
+# if temperature >30:
+#     print("It\'s warm")
 
+# Else if
+# age=int(input("Enter your age: "))
+# if age>18:
+#     print("You are eligible to vote")
+# elif age==18:
+#     print("You can vote next year")
+# else:
+#     print("You are not eligible to vote")
+
+# Logical operator
+heigh_income=True
+good_credit=True
+# if heigh_income and good_credit:
+#     print("Eligible")
+# else:
+#     print("You are not eligible")
+
+# another example
+# student=False
+# if (heigh_income or good_credit) and not student:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# Age should be between 18 and 65
+# age=22
+# if age>=18 and age<65: 
+#     print("Eligible")
+
+# if 18<=age<65: # Easily implement mathematical expressions
+#     print("Eligible")
+
+# Quiz
+# if 10=="10":
+#     print("a")
+# elif "bag">"apple" and "bag">"cat":
+#     print("b")
+# else:
+#     print("c")
+
+# for loop
+# for number in range(3):
+#     print(number)
+
+# for number in range(1,4):
+#     print("Attempt", number, number*".")
+
+# for else
+# break => break is used to terminate the loop
+for number in range(1,10):
+    print("Hello")
 
 # Exercise 1
 # Print prime number from 1-10
-count=0
-for x in range(1,10):
-    if x%2==0:
-        count+=1
-        print(x)
-print(f"We have {count} even numbers")
+# count=0
+# for x in range(1,10):
+#     if x%2==0:
+#         count+=1
+#         print(x)
+# print(f"We have {count} even numbers")
