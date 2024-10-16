bb68cedb13a84d58623feee981a8eb32e71ec162
nabin-8 <nabinacharya740@gmail.com>
2024-10-16 22:57:55 +0545

    abs class abstract

--- Complete_Python_Django/Python/07_Classes.py
+++ Complete_Python_Django/Python/07_Classes.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 # class Point:
 #     def __init__(self,x,y):
 #         self.x=x
@@ -39,33 +40,117 @@
 # Creating custom data structure / container
 class TagCloud:
     def __init__(self):
-        self.tags={}
+        self.__tags={}
     
     def add(self, tag):
-        self.tags[tag.lower()]=self.tags.get(tag.lower(),0)+1
+        self.__tags[tag.lower()]=self.__tags.get(tag.lower(),0)+1
     
     def __getitem__(self, tag):
-        return self.tags.get(tag.lower(),0)
+        return self.__tags.get(tag.lower(),0)
     
     def __setitems__(self, tag, count):
-        self.tags[tag.lower()]=count
+        self.__tags[tag.lower()]=count
         
     def __len__(self):
-        return len(self.tags)
+        return len(self.__tags)
     
     def __iter__(self):
-        return iter(self.tags)
+        return iter(self.__tags)
 
 cloud=TagCloud()
-cloud.add("Python")
-cloud.add("python")
-# cloud["python"]=10
-cloud.add("python")
-cloud.add("nabin")
-cloud.add("Nabin")
-cloud.add("python")
-cloud.add("python")
-print(cloud.tags)
-# cloud=TagCloud()
-print(len(cloud))
-# cloud["python"]=10 
+# cloud.add("Python")
+# cloud.add("python")
+# # cloud["python"]=10
+# cloud.add("python")
+# cloud.add("nabin")
+# cloud.add("Nabin")
+# cloud.add("python")
+# cloud.add("python")
+# print(cloud.__tags)
+# print(len(cloud))
+
+# print(cloud.__dict__)
+# print(cloud._TagCloud__tags)
+
+
+class Product:
+    def __init__(self, price):
+        self.price = price
+    
+    @property
+    def price(self):
+        return self.__price
+    
+    @price.setter
+    def price(self, value):
+        if value<0:
+            raise ValueError("Price cannot br negative.")
+        self.__price=value
+
+
+# product=Product(-10)
+# print(product.price)
+
+
+
+class Animal:
+    def __init__(self):
+        self.age=1
+    
+    def eat(self):
+        print("eat")
+
+# Animal: Parent, Base class
+# Mammal: Child, Sub class
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+        
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+# m=Mammal()
+# m.eat()
+# print(m.age)
+
+# f=Fish()
+# f.swim()
+
+
+# A Good example of inheritance
+class InvalidOperationError(Exception):
+    pass
+
+class Stream(ABC):
+    def __init__(self):
+        self.opened = False
+    
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already open.")
+        self.opened=True   
+        
+    def close(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already close.")
+        self.opened=False
+    
+    @abstractmethod
+    def read(self):
+        pass
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading data from a file")
+        
+        
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from a network")
+        
+
+stream=Stream()
+stream.open
