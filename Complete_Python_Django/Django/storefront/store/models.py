f3c04ccfb4f42f80b08b39b00ce6e3f4adff7d58
nabin-8 <nabinacharya740@gmail.com>
2024-10-18 23:27:05 +0545

    django migrations

--- Complete_Python_Django/Django/storefront/store/models.py
+++ Complete_Python_Django/Django/storefront/store/models.py
@@ -9,12 +9,15 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    feature_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
     
     
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now =True)
     collections=models.ForeignKey(
