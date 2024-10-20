b20e4fe625f2da9a5c5364f2334098d65cf78c9a
nabin-8 <nabinacharya740@gmail.com>
2024-10-20 15:59:41 +0545

    complex filtering

--- Complete_Python_Django/Django/storefront/playground/views.py
+++ Complete_Python_Django/Django/storefront/playground/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 def say_hello(request):
     # return HttpResponse('Hello World')
-    return render(request, 'hello.html',{'name':'Nabin'})
+    # try:
+        
+    #     query_set = Product.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
+    
+    # product = Product.objects.filter(pk=0).first()
+    queryset=Product.objects.filter(last_update__year=2021)
+    
+    # for product in query_set: 
+    #     print(product)
+    return render(request, 'hello.html',{'name':'Nabin', 'products':list(queryset)}) 
